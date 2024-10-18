eadd9959bde6e984b1f83170b36954fce7cb84c6
Tlosz-cmd <lakobabo345@gmail.com>
2024-10-18 11:08:39 +0200

    igen

---
+++ data.js
@@ -0,0 +1,29 @@
+const kerdesek = [
+    {
+        kerdes: 'Mi Magyarország fővárosa?',
+        valasz: [
+            { text: 'Budapest', correct: true },
+            { text: 'Pécs', correct: false },
+            { text: 'Debrecen', correct: false },
+            { text: 'Szeged', correct: false }
+        ]
+    },
+    {
+        kerdes: 'Mi a legnagyobb bolygó a Naprendszerben?',
+        valasz: [
+            { text: 'Föld', correct: false },
+            { text: 'Mars', correct: false },
+            { text: 'Jupiter', correct: true },
+            { text: 'Vénusz', correct: false }
+        ]
+    },
+    {
+        kerdes: 'Ki festette a Mona Lisát?',
+        valasz: [
+            { text: 'Vincent van Gogh', correct: false },
+            { text: 'Leonardo da Vinci', correct: true },
+            { text: 'Michelangelo', correct: false },
+            { text: 'Pablo Picasso', correct: false }
+        ]
+    }
+];
